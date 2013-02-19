7556d8e6c983602a20065205ba2ceb3b53866bca
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-18 19:08:33 -0500

    Solution found, takes about 3 min to solve

--- Highly-Divisible-Triangular-Number.py
+++ Highly-Divisible-Triangular-Number.py
@@ -1,18 +1,46 @@
 num = 1
 triangleNum = 0
-
-def DivisorCount(numCheck):
-    divisors = 0
-    for x in range(1 , (numCheck + 1)):
-        if (numCheck % x) == 0:
-            divisors += 1
-    return divisors
+import math
     
+def FactorCount(numCheck):
+    primeCount = []
+    tempNum = numCheck
+    testPrime = 2
+    counter = 1
+    returnTotal = 1
+    while testPrime <= math.sqrt(numCheck):
+        if tempNum % testPrime == 0:
+            counter += 1
+            tempNum = tempNum / testPrime
+        else:
+            primeCount.append(counter)
+            testPrime = NextPrime(testPrime)
+            counter = 1
+    for x in primeCount:
+        returnTotal = returnTotal * x
+    return returnTotal
+
+def CheckPrime(numCheck):
+    i = 2
+    maxCheck = math.sqrt(numCheck)
+    while i <= maxCheck:
+        if numCheck % i == 0:
+            return 0
+        i += 1
+    return 1
+
+def NextPrime(numCheck):
+    i = numCheck
+    while True:
+        i += 1
+        if(CheckPrime(i)):
+            return i
+            
 while True:
     triangleNum = triangleNum + num
-    if DivisorCount(triangleNum) > 3:
+    if FactorCount(triangleNum) > 500:
         break;
     else:
         num += 1
-        
+
 print triangleNum
